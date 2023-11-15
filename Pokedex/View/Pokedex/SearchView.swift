59ab8ec9f29af886e88c0983f39add6a8ebea760
Nathat Kuanthanom <nkpeam51@gmail.com>
2023-11-15 23:53:48 +0700

    add search feature
    - add no result view
    - able to search pokemon from pokemon name

--- Pokedex/View/Pokedex/SearchView.swift
+++ Pokedex/View/Pokedex/SearchView.swift
@@ -9,15 +9,71 @@ import SwiftUI
 
 struct SearchView: View {
     @State private var searchText = ""
+    @EnvironmentObject var pokemonViewModel: PokemonViewModel
+    @State private var pokemonData: [Pokemon]? = nil
+    
+    let twoColumns = [
+            GridItem(.fixed(170), spacing: 15),
+            GridItem(.fixed(170), spacing: 15)
+            
+        ]
+    
     var body: some View {
-        VStack{
-         Text("\(searchText)")
+        ScrollView{
+            // display nothing
+            if(searchText.isEmpty){
+                
+            }
+            // searched pokemon not found
+            else if(filteredPokemon.isEmpty){
+                VStack{
+                    Image("pikachu-back")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width:230, height: 120)
+                    Text("No result for ")
+                        .font(.title2)
+                    Text("\" \(searchText) \"")
+                        .frame(width: 200)
+                        .font(.title3)
+                }.frame(width: 300, height: 300)
+                    .padding(.vertical, 100)
+                
+            }
+            // display searched pokemon
+            else{
+                LazyVGrid(columns: twoColumns, spacing: 15,content: {
+                    ForEach(filteredPokemon){ pokemon in
+                        PokemonComponent(pokemon: pokemon)
+                        
+                    }
+                })
+            }
         }
-        .searchable(text: $searchText)
-        .navigationBarBackButtonHidden()
+        .searchable(text: $searchText, prompt: "Please enter pokemon name")
+        .task{
+            do{
+                pokemonData = try await pokemonViewModel.fetchPokemon()
+            }catch{
+                print("Failed to fetching data with error: \(error)")
+            }
+        }
+    }
+}
+
+extension SearchView{
+    var filteredPokemon: [Pokemon]{
+        if searchText.isEmpty{
+            return []
+        }else{
+            return pokemonData?.filter { $0.name.starts(with: searchText)} ?? []
+        }
     }
 }
 
 #Preview {
-    SearchView()
+    NavigationStack{
+        SearchView().environmentObject(PokemonViewModel())
+    }
+
 }
