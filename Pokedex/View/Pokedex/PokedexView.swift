7d4d5535e852957b0758c2aef0f45751f2fe8208
Nathat Kuanthanom <nkpeam51@gmail.com>
2023-11-15 18:10:27 +0700

    add pull to refresh feature
    - can be pulled to refresh
    - add error screen

--- Pokedex/View/Pokedex/PokedexView.swift
+++ Pokedex/View/Pokedex/PokedexView.swift
@@ -11,6 +11,7 @@ struct PokedexView: View {
     @EnvironmentObject var pokemonViewModel: PokemonViewModel
     @State private var pokemonData: [Pokemon]? = nil
     @State private var isDisplayTwoColums: Bool = true
+    @State private var isFetching: Bool = true
 
     let twoColumns = [
             GridItem(.fixed(170), spacing: 15),
@@ -25,9 +26,31 @@ struct PokedexView: View {
     ]
     var body: some View {
         NavigationStack{
-            if let pokemonData = pokemonData{
-                ScrollView{
-                    Spacer().frame(height: 15)
+            ScrollView{
+                Spacer().frame(height: 15)
+                if(isFetching){
+                    VStack(alignment: .center) {
+                        Text("Loading")
+                            .font(.headline)
+                            .foregroundStyle(Color(red: 0.957, green: 0.455, blue: 0.455))
+                            .padding(.vertical, 100)
+                            .task {
+                            do {
+                                pokemonData = try await pokemonViewModel.fetchPokemon()
+                                sleep(UInt32(3.0))
+                                print("Fetched Pokemon")
+                                print(pokemonData![0])
+                            } catch {
+                                // Handle the error here
+                                print("Error fetching pokemon: \(error)")
+                                
+                            }
+                            isFetching = false
+                        }
+                    }
+                    
+                }
+                else if let pokemonData = pokemonData{
                     LazyVGrid(columns: isDisplayTwoColums ? twoColumns: threeColumns, spacing: 15,content: {
                         ForEach(pokemonData, id: \.name) { pokemon in
                             
@@ -40,54 +63,44 @@ struct PokedexView: View {
                             }
                         }
                     })
+                }else{
+                    VStack{
+                        
+                        Image("pikachu-curious")
+                            .resizable()
+                            .frame(width: 150, height: 150)
+                        Text("Opps").bold()
+                        Text("Failed to fetching data").font(.subheadline)
+                    }.padding(50)
                 }
-                .frame(maxWidth: .infinity, maxHeight:.infinity)
-
-                .navigationTitle("Pokedex").navigationBarTitleDisplayMode(.inline)
-                .toolbar(content: {
-                                ToolbarItem(placement: .navigationBarLeading) {
-                                    Button(action: {
-                                        isDisplayTwoColums.toggle()
-                                    }, label: {
-                                        Image(systemName: isDisplayTwoColums ? "square.grid.3x3" : "rectangle.grid.2x2" )
-                                            .foregroundStyle(Color(red: 0.957, green: 0.455, blue: 0.455))
-                                    })
-                                    
-                                }
-                                ToolbarItem(placement: .navigationBarTrailing) {
-                                    NavigationLink(destination: {
-                                        SearchView()
-                                    }, label: {
-                                        Image(systemName: "magnifyingglass")
-                                            .foregroundStyle(Color(red: 0.957, green: 0.455, blue: 0.455))
-                                    })
-                                }
+                
+            }
+            .refreshable {
+                isFetching.toggle()
+            }
+            .frame(maxWidth: .infinity, maxHeight:.infinity)
+            .navigationTitle("Pokedex").navigationBarTitleDisplayMode(.inline)
+            .toolbar(content: {
+                            ToolbarItem(placement: .navigationBarLeading) {
+                                Button(action: {
+                                    isDisplayTwoColums.toggle()
+                                }, label: {
+                                    Image(systemName: isDisplayTwoColums ? "square.grid.3x3" : "rectangle.grid.2x2" )
+                                        .foregroundStyle(Color(red: 0.957, green: 0.455, blue: 0.455))
+                                })
+                                
                             }
-                )
-            }
-            
-            else{
-                ProgressView().task {
-                    do {
-                        pokemonData = try await pokemonViewModel.fetchPokemon()
-                        print("Fetched Pokemon")
-                        print(pokemonData![0])
-                    } catch {
-                        // Handle the error here
-                        print("Error fetching pokemon: \(error)")
-                    }
-                }
-            }
-            
-            
-            
-            
-            
-            
+                            ToolbarItem(placement: .navigationBarTrailing) {
+                                NavigationLink(destination: {
+                                    SearchView()
+                                }, label: {
+                                    Image(systemName: "magnifyingglass")
+                                        .foregroundStyle(Color(red: 0.957, green: 0.455, blue: 0.455))
+                                })
+                            }
+                        }
+            )
         }
-        
-        
-
     }
 }
 
