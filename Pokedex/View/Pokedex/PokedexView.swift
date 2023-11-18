1881be3b2fe4d83572c5bc8dd364b415922f38c2
Nathat Kuanthanom <nkpeam51@gmail.com>
2023-11-18 16:54:16 +0700

    added loading screen to pokedex view

--- Pokedex/View/Pokedex/PokedexView.swift
+++ Pokedex/View/Pokedex/PokedexView.swift
@@ -28,80 +28,81 @@ struct PokedexView: View {
     ]
     var body: some View {
         NavigationStack{
-            ScrollView{
-                Spacer().frame(height: 15)
+            
+            ZStack {
+                // display progress view when fetching data
                 if(isFetching){
-                    VStack(alignment: .center) {
-                        Text("Loading")
-                            .font(.headline)
-                            .foregroundStyle(Color(red: 0.957, green: 0.455, blue: 0.455))
-                            .padding(.vertical, 100)
-                            .task {
-                            do {
-                                pokemonData = try await pokemonViewModel.fetchPokemon()
-                                sleep(UInt32(3.0))
-                                print("Fetched Pokemon")
-                                print(pokemonData![0])
-                            } catch {
-                                // Handle the error here
-                                print("Error fetching pokemon: \(error)")
-                                
-                            }
-                            isFetching = false
+                    PokeballProgressView().task {
+                        do {
+                            pokemonData = try await pokemonViewModel.fetchPokemon()
+                            sleep(UInt32(3.0))
+                            print("Fetched Pokemon")
+                            print(pokemonData![0])
+                        } catch {
+                            // Handle the error here
+                            print("Error fetching pokemon: \(error)")
+                            
                         }
+                        isFetching = false
+                    }
+                }
+                ScrollView{
+                    Spacer().frame(height: 15)
+                    if(isFetching){
+ 
+                    }
+                    // display pokemon data collection
+                    else if let pokemonData = pokemonData{
+                        LazyVGrid(columns: isDisplayTwoColums ? twoColumns: threeColumns, spacing: 15,content: {
+                            ForEach(pokemonData, id: \.name) { pokemon in
+                                
+                                if(isDisplayTwoColums){
+                                    PokemonComponent(pokemon: pokemon).environmentObject(pokemonViewModel)
+                                }
+                                else{
+                                    // display grid 3 colums
+                                    PokemonImageComponent(pokemon: pokemon).environmentObject(pokemonViewModel)
+                                }
+                            }
+                        })
+                    }else{
+                        VStack{
+                            
+                            Image("pikachu-curious")
+                                .resizable()
+                                .frame(width: 150, height: 150)
+                            Text("Opps").bold()
+                            Text("Failed to fetching data").font(.subheadline)
+                        }.padding(50)
                     }
                     
                 }
-                else if let pokemonData = pokemonData{
-                    LazyVGrid(columns: isDisplayTwoColums ? twoColumns: threeColumns, spacing: 15,content: {
-                        ForEach(pokemonData, id: \.name) { pokemon in
-                            
-                            if(isDisplayTwoColums){
-                                PokemonComponent(pokemon: pokemon).environmentObject(pokemonViewModel)
-                            }
-                            else{
-                                // display grid 3 colums
-                                PokemonImageComponent(pokemon: pokemon).environmentObject(pokemonViewModel)
-                            }
-                        }
-                    })
-                }else{
-                    VStack{
-                        
-                        Image("pikachu-curious")
-                            .resizable()
-                            .frame(width: 150, height: 150)
-                        Text("Opps").bold()
-                        Text("Failed to fetching data").font(.subheadline)
-                    }.padding(50)
+                .refreshable {
+                    isFetching.toggle()
                 }
-                
-            }
-            .refreshable {
-                isFetching.toggle()
-            }
-            .frame(maxWidth: .infinity, maxHeight:.infinity)
-            .navigationTitle("Pokedex").navigationBarTitleDisplayMode(.inline)
-            .toolbar(content: {
-                            ToolbarItem(placement: .navigationBarLeading) {
-                                Button(action: {
-                                    isDisplayTwoColums.toggle()
-                                }, label: {
-                                    Image(systemName: isDisplayTwoColums ? "square.grid.3x3" : "rectangle.grid.2x2" )
-                                        .foregroundStyle(Color(red: 0.957, green: 0.455, blue: 0.455))
-                                })
-                                
+                .frame(maxWidth: .infinity, maxHeight:.infinity)
+                .navigationTitle("Pokedex").navigationBarTitleDisplayMode(.inline)
+                .toolbar(content: {
+                                ToolbarItem(placement: .navigationBarLeading) {
+                                    Button(action: {
+                                        isDisplayTwoColums.toggle()
+                                    }, label: {
+                                        Image(systemName: isDisplayTwoColums ? "square.grid.3x3" : "rectangle.grid.2x2" )
+                                            .foregroundStyle(Color(red: 0.957, green: 0.455, blue: 0.455))
+                                    })
+                                    
+                                }
+                                ToolbarItem(placement: .navigationBarTrailing) {
+                                    NavigationLink(destination: {
+                                        SearchView().environmentObject(pokemonViewModel)
+                                    }, label: {
+                                        Image(systemName: "magnifyingglass")
+                                            .foregroundStyle(Color(red: 0.957, green: 0.455, blue: 0.455))
+                                    })
+                                }
                             }
-                            ToolbarItem(placement: .navigationBarTrailing) {
-                                NavigationLink(destination: {
-                                    SearchView().environmentObject(pokemonViewModel)
-                                }, label: {
-                                    Image(systemName: "magnifyingglass")
-                                        .foregroundStyle(Color(red: 0.957, green: 0.455, blue: 0.455))
-                                })
-                            }
-                        }
             )
+            }
         }
     }
 }
